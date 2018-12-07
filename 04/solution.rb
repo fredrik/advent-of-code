acb1e9749df9d8db0cfdaec7608982fbd4b99975
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2018-12-07 13:27:54 +0100

    Solve 04 part one

---
+++ 04/solution.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+def parse(line)
+  # Example: "[1518-11-17 00:02] Guard #1069 begins shift"
+  minute, text = line.scan(/ \d\d:(\d\d)\] (.+)/).first
+  guard_id = text.scan(/Guard \#(\d+)/).first
+  minute = minute.to_i
+
+  if guard_id
+    action = :begin
+    guard_id = Integer(guard_id.first)
+  elsif text == 'falls asleep'
+    action = :sleep
+  elsif text == 'wakes up'
+    action = :wake
+  else
+    raise "Bad input"
+  end
+
+  [action, minute, guard_id]
+end
+
+sleep_times = []
+guard_on_duty = nil
+asleep_at = nil
+
+ARGF.sort.each do |line|
+  action, minute, guard_id = parse(line)
+
+  case action
+  when :begin
+    guard_on_duty = guard_id if action == :begin
+  when :sleep
+    asleep_at = minute
+  when :wake
+    (asleep_at..(minute-1)).each do |m|
+      sleep_times << [guard_on_duty, m]
+    end
+  end
+end
+
+minutes_per_guard = Hash.new(0)
+
+sleep_times.each do |guard_id, minute|
+  minutes_per_guard[guard_id] += 1
+end
+
+sleepiest_guard = minutes_per_guard.sort_by { |guard_id, minutes| -minutes }.first.first
+
+# but when did he sleep?
+
+most_slept_minute = sleep_times
+  .select { |guard_id, _| guard_id == sleepiest_guard }.map(&:last)
+  .sort.chunk { |n| n }
+  .map { |n, ns| [n, ns.size] }
+  .sort_by { |n, size| -size }
+  .first.first
+
+
+puts "sleepiest: #{sleepiest_guard}"
+puts "most slept minute: #{most_slept_minute}"
+puts "=> #{most_slept_minute * sleepiest_guard}"
