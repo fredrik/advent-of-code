d9a3e4a84e9c1254d797aaa522662404e938edff
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2024-12-05 22:27:27 +0100

    refactor d05

--- 2024/05/d05.py
+++ 2024/05/d05.py
@@ -4,77 +4,63 @@ from collections import defaultdict
 
 def part1(input):
     rules, updates = parse_input(input)
-    # print(f"rules: {rules}")
-    # print(f"updates: {updates}")
 
     sum = 0
     for update in updates:
-        valid_update = True
-        seen = set()
-        for i in range(len(update)):
-            item = update[i]
-            seen.add(item)
-            if rules[item] & seen:
-                valid_update = False
-                break
-        if valid_update:
-            # update is valid. get middle number.
-            sum += int(update[int(len(update) / 2)])
-
-    print(sum)
+        if valid_update(update, rules):
+            sum += middle_element(update)
+    return sum
 
 
 def part2(input):
     rules, updates = parse_input(input)
-    # print(f"rules: {rules}")
-    # print(f"updates: {updates}")
 
     sum = 0
     for update in updates:
-        valid_update = True
-        seen = set()
-        for i in range(len(update)):
-            item = update[i]
-            seen.add(item)
-            if rules[item] & seen:
-                valid_update = False
-                break
-
-        if not valid_update:
-            reordered = reorder(update, rules)
-            sum += int(reordered[int(len(reordered) / 2)])
-            # print(f"invalid update: {update}")
-            # print(f"reordered: {reordered}")
-            # print()
-
-    print(sum)
+        if not valid_update(update, rules):
+            reordered = order_correctly(update, rules)
+            print(f"reordered: {reordered}")
+            sum += middle_element(reordered)
+    return sum
 
 
-def reorder(update, rules):
-    for i in range(len(update) - 1):
+def valid_update(update, rules):
+    seen = set()
+    for i in range(len(update)):
         item = update[i]
-        next_item = update[i + 1]
-        # print(f"item: {item}, next_item: {next_item}, itemrules: {rules[item]}, nextitemrules: {rules[next_item]}")
+        seen.add(item)
+        if rules[item] & seen:
+            return False
+    else:
+        return True
+
+
+def order_correctly(update, rules):
+    def wrong_order(item, next_item, rules):
+        return rules[next_item] & {item}
+
+    for i in range(len(update) - 1):
+        item, next_item = update[i], update[i + 1]
         if wrong_order(item, next_item, rules):
-            # swap
-            update[i], update[i + 1] = next_item, item
-            return reorder(update, rules)
+            update[i], update[i + 1] = next_item, item  # swap
+            return order_correctly(update, rules)
 
     return update
 
 
-def wrong_order(item, next_item, rules):
-    return rules[next_item] & {item}
+def middle_element(update):
+    return int(update[len(update) // 2])
 
 
 def parse_input(input):
     rules_raw, updates_raw = input.strip().split("\n\n")
 
     rules = defaultdict(set)
-    updates = []
     for rule in rules_raw.strip().splitlines():
         left, right = rule.split("|")
         rules[left.strip()].add(right.strip())
+
+    updates = []
     for update in updates_raw.strip().splitlines():
         updates.append(update.strip().split(","))
 
@@ -86,11 +72,11 @@ def choose_input():
         with open("input.txt", "r") as f:
             return f.read()
     else:
-        with open("input.small", "r") as f:
+        with open("input.mini", "r") as f:
             return f.read()
 
 
 if __name__ == "__main__":
     input = choose_input()
-    part1(input)
-    part2(input)
+    print(part1(input))
+    print(part2(input))
