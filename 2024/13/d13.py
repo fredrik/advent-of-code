8c64d8f2af6f327ec6fecc13022c6da3d02de8a2
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2024-12-15 11:26:19 +0100

    egads

--- 2024/13/d13.py
+++ 2024/13/d13.py
@@ -21,7 +21,7 @@ def solve(input, part2):
     costs = []
     for index, machine in enumerate(machines):
         print("machine", index)
-        solutions = find_solution(machine)
+        solutions = find_solution(machine, 10000000000000 if part2 else 0)
         if len(solutions) > 1:
             print("multiple solutions!@#")
         for m, n, cost in solutions:
@@ -33,16 +33,33 @@ def solve(input, part2):
     return sum(costs)
 
 
-def find_solution(machine):
-    solutions = []
-    for i in range(100):
-        for j in range(100):
-            if (
-                machine["prize"][0] == i * machine["a"][0] + j * machine["b"][0]
-                and machine["prize"][1] == i * machine["a"][1] + j * machine["b"][1]
-            ):
-                solutions.append((i, j, cost["a"] * i + cost["b"] * j))
-    return solutions
+def find_solution(machine, prize_offset):
+    # solutions = []
+    # for i in range(100000):
+    #     for j in range(100000):
+    #         if (
+    #             prize_offset + machine["prize"][0] == i * machine["a"][0] + j * machine["b"][0]
+    #             and prize_offset + machine["prize"][1] == i * machine["a"][1] + j * machine["b"][1]
+    #         ):
+    #             solutions.append((i, j, cost["a"] * i + cost["b"] * j))
+    # return solutions
+
+    px = prize_offset + machine["prize"][0]
+    py = prize_offset + machine["prize"][1]
+    ax = machine["a"][0]
+    ay = machine["a"][1]
+    bx = machine["b"][0]
+    by = machine["b"][1]
+
+    i = (px * by - py * bx) / (ax * by - ay * bx)
+    j = (py * ax - px * by) / (ax * by - ay * bx)
+
+    if (i == int(i)) and (j == int(j)):
+        return [(i, j, cost["a"] * i + cost["b"] * j)]
+    else:
+        print(i, int(i))
+        print(j, int(j))
+        return []
 
 
 def parse_input(input):
@@ -71,5 +88,5 @@ def choose_input():
 
 if __name__ == "__main__":
     input = choose_input()
-    print("part 1", solve(input, False))
-    # print("part 2", solve(input, True))
+    # print("part 1", solve(input, False))
+    print("part 2", solve(input, True))
