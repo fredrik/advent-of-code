c5da207c24840e6893a251ec8b467e0c0e4e8c43
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2024-12-17 09:04:26 +0100

    d13 part 2

--- 2024/13/d13.py
+++ 2024/13/d13.py
@@ -20,30 +20,15 @@ def solve(input, part2):
 
     costs = []
     for index, machine in enumerate(machines):
-        print("machine", index)
         solutions = find_solution(machine, 10000000000000 if part2 else 0)
-        if len(solutions) > 1:
-            print("multiple solutions!@#")
-        for m, n, cost in solutions:
-            print(m, n, cost)
-            costs.append(cost)
 
-        print()
+        for m, n, cost in solutions:
+            costs.append(cost)
 
     return sum(costs)
 
 
 def find_solution(machine, prize_offset):
-    # solutions = []
-    # for i in range(100000):
-    #     for j in range(100000):
-    #         if (
-    #             prize_offset + machine["prize"][0] == i * machine["a"][0] + j * machine["b"][0]
-    #             and prize_offset + machine["prize"][1] == i * machine["a"][1] + j * machine["b"][1]
-    #         ):
-    #             solutions.append((i, j, cost["a"] * i + cost["b"] * j))
-    # return solutions
-
     px = prize_offset + machine["prize"][0]
     py = prize_offset + machine["prize"][1]
     ax = machine["a"][0]
@@ -52,13 +37,11 @@ def find_solution(machine, prize_offset):
     by = machine["b"][1]
 
     i = (px * by - py * bx) / (ax * by - ay * bx)
-    j = (py * ax - px * by) / (ax * by - ay * bx)
+    j = (py * ax - px * ay) / (ax * by - ay * bx)
 
     if (i == int(i)) and (j == int(j)):
-        return [(i, j, cost["a"] * i + cost["b"] * j)]
+        return [(int(i), int(j), int(cost["a"] * i + cost["b"] * j))]
     else:
-        print(i, int(i))
-        print(j, int(j))
         return []
 
 
@@ -88,5 +71,5 @@ def choose_input():
 
 if __name__ == "__main__":
     input = choose_input()
-    # print("part 1", solve(input, False))
+    print("part 1", solve(input, False))
     print("part 2", solve(input, True))
