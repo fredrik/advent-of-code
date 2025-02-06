97691348f9ebd47ea9713b1a919009c18c770d89
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-02-06 19:42:21 +0100

    refactor d16 using dijkstra

--- 2024/16/d16.py
+++ 2024/16/d16.py
@@ -1,5 +1,7 @@
+import heapq
 import os
-import networkx as nx
+
+from collections import defaultdict
 
 
 EAST = (0, 1)
@@ -19,14 +21,64 @@ def solve(input, part):
 
 
 def part1(graph, start, end):
-    path = nx.shortest_path(graph, (start, EAST), end, weight="weight")
-    return nx.path_weight(graph, path, "weight")
+    shortest_length, shortest_paths = find_shortest_paths(graph, (start, EAST), end)
+    return shortest_length
 
 
 def part2(graph, start, end, grid):
-    paths = nx.all_shortest_paths(graph, (start, EAST), end, weight="weight")
-    seats = set(node for path in paths for node, _ in path[:-1])
+    shortest_length, shortest_paths = find_shortest_paths(graph, (start, EAST), end)
+    seats = set(node for path in shortest_paths for node, _ in path[:-1])
     return len(seats)
+
+
+# ---
+
+
+# dijsktra's algorithm.
+def find_shortest_paths(graph, start, end):
+    distances = {node: float("inf") for node in graph}
+    distances[start] = 0
+    predecessors = defaultdict(list)
+
+    def reconstruct_all_paths(node):
+        if node == start:
+            return [[start]]
+
+        paths = []
+        for pre in predecessors[node]:
+            for path in reconstruct_all_paths(pre):
+                paths.append(path + [node])
+
+        return paths
+
+    # priority queue.
+    pq = [(0, start)]
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        if current_node == end:
+            continue
+
+        for neighbour, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbour]:
+                distances[neighbour] = distance
+                heapq.heappush(pq, (distance, neighbour))
+                predecessors[neighbour] = [current_node]
+
+            if distance == distances[neighbour]:
+                if current_node not in predecessors[neighbour]:
+                    predecessors[neighbour].append(current_node)
+
+    if distances[end] == float("inf"):
+        return None, []
+
+    return distances[end], reconstruct_all_paths(end)
 
 
 # ---
@@ -46,39 +98,39 @@ def parse_grid(input):
 
 
 def make_graph(grid):
-    graph = nx.DiGraph()
+    graph = defaultdict(dict)
+
+    def add_edge(node, neighbour, weight):
+        graph[node][neighbour] = weight
 
     for point in grid:
         if grid[point] == "#":
             continue
 
         if grid[point] == "E":
-            # end edge-case
-            graph.add_node(point)
-            graph.add_edge((point, EAST), point, weight=0)
-            graph.add_edge((point, WEST), point, weight=0)
-            graph.add_edge((point, NORTH), point, weight=0)
-            graph.add_edge((point, SOUTH), point, weight=0)
+            # edge-case for end of graph.
+            # it doesn't matter which way we're facing.
+            graph[point] = {}
+            add_edge((point, EAST), point, weight=0)
+            add_edge((point, WEST), point, weight=0)
+            add_edge((point, NORTH), point, weight=0)
+            add_edge((point, SOUTH), point, weight=0)
+            continue
 
-        # add a node for each direction we can face.
-        graph.add_node((point, EAST))
-        graph.add_node((point, WEST))
-        graph.add_node((point, NORTH))
-        graph.add_node((point, SOUTH))
-
-        # add edges for turning.
-        graph.add_edge((point, EAST), (point, NORTH), weight=1000)
-        graph.add_edge((point, EAST), (point, SOUTH), weight=1000)
-        graph.add_edge((point, WEST), (point, NORTH), weight=1000)
-        graph.add_edge((point, WEST), (point, SOUTH), weight=1000)
-        graph.add_edge((point, NORTH), (point, WEST), weight=1000)
-        graph.add_edge((point, NORTH), (point, EAST), weight=1000)
-        graph.add_edge((point, SOUTH), (point, EAST), weight=1000)
-        graph.add_edge((point, SOUTH), (point, WEST), weight=1000)
+        # add expensively weighted edges for turning.
+        add_edge((point, EAST), (point, NORTH), weight=1000)
+        add_edge((point, EAST), (point, SOUTH), weight=1000)
+        add_edge((point, WEST), (point, NORTH), weight=1000)
+        add_edge((point, WEST), (point, SOUTH), weight=1000)
+        add_edge((point, NORTH), (point, WEST), weight=1000)
+        add_edge((point, NORTH), (point, EAST), weight=1000)
+        add_edge((point, SOUTH), (point, EAST), weight=1000)
+        add_edge((point, SOUTH), (point, WEST), weight=1000)
 
         for p, d in neighbours(grid, point):
             # point and direction of reachable neighbour.
-            graph.add_edge((point, d), (p, d), weight=1)
+            # this is moving 'forward', so the weight is 1.
+            add_edge((point, d), (p, d), weight=1)
 
     return graph
 
@@ -86,27 +138,26 @@ def make_graph(grid):
 def neighbours(grid, point):
     directions = [EAST, WEST, NORTH, SOUTH]
     r, c = point
-    for d in directions:
-        dr, dc = d
+    for dr, dc in directions:
         n = (r + dr, c + dc)
         if grid.get(n) in [".", "E"]:
-            yield n, d
+            yield n, (dr, dc)
 
 
-# --- input basics ---
+# ---
 
 
-def choose_input():
-    if os.environ.get("LARGE_INPUT"):
-        filename = "input.txt"
+def get_input_data():
+    if os.environ.get("INPUT"):
+        filename = os.environ.get("INPUT")
     else:
-        filename = "input.small"
+        filename = "input.txt"
 
     with open(filename, "r") as f:
         return f.read()
 
 
 if __name__ == "__main__":
-    input = choose_input()
-    print("part 1", solve(input, 1))
-    print("part 2", solve(input, 2))
+    data = get_input_data()
+    print("part 1:", solve(data, 1))
+    print("part 2:", solve(data, 2))
