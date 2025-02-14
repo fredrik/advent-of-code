a8c577b18a72ad59f64da1cd3e47af2a14495f05
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-02-14 15:48:02 +0100

    Refactor d20 using BFS

--- 2024/20/d20.py
+++ 2024/20/d20.py
@@ -1,13 +1,11 @@
 import itertools
-
-import networkx
+from collections import deque, defaultdict
 
 
 def solve(input, part, diff_cutoff):
-    nodes, edges, start, end = parse_input(input)
+    graph, start, end = parse_input(input)
 
-    graph = networkx.Graph(edges)
-    paths = dict(networkx.shortest_path_length(graph, target=end))
+    distances = find_distances(graph, end)
 
     if part == 1:
         max_jump_length = 2
@@ -15,23 +13,42 @@ def solve(input, part, diff_cutoff):
         max_jump_length = 20
 
     count = 0
-    for x, y in graph.nodes:
-        for nx, ny in jumps(nodes, x, y, max_jump_length):
+    for x, y in graph:
+        for nx, ny in jumps(graph, x, y, max_jump_length):
             jump_length = abs(x - nx) + abs(y - ny)
-            diff = paths[(x, y)] - paths[(nx, ny)] - jump_length
+            diff = distances[(x, y)] - distances[(nx, ny)] - jump_length
             if diff >= diff_cutoff:
                 count += 1
     return count
 
 
-def jumps(nodes, x, y, jump):
+# bfs.
+def find_distances(graph, target):
+    distances = defaultdict(lambda: float("inf"))
+    distances[target] = 0
+
+    queue = deque([target])
+    visited = set([target])
+
+    while queue:
+        current = queue.popleft()
+
+        for neighbour in graph[current]:
+            if neighbour not in visited:
+                queue.append(neighbour)
+                visited.add(neighbour)
+                distances[neighbour] = distances[current] + 1
+    return distances
+
+
+def jumps(graph, x, y, jump):
     i = range(-jump, jump + 1)
     j = range(-jump, jump + 1)
     prod = itertools.product(i, j)
     for dx, dy in [(x, y) for x, y in prod if abs(x) + abs(y) <= jump and (x, y) != (0, 0)]:
         nx, ny = x + dx, y + dy
-        if (nx, ny) in nodes and nodes[(nx, ny)] != "#":
-            yield nx, ny
+        if (nx, ny) in graph: # avoid walls by checking that the jump lands on a node.
+            yield (nx, ny)
 
 
 # ---
@@ -40,8 +57,8 @@ def jumps(nodes, x, y, jump):
 def parse_input(input):
     lines = input.strip().split("\n")
 
-    nodes = {}
-    edges = set()
+    graph = defaultdict(set)  # adjacency
+
     for y, row in enumerate(lines):
         for x, cell in enumerate(row):
             if cell == "#":
@@ -50,19 +67,19 @@ def parse_input(input):
                 start_position = (x, y)
             if cell == "E":
                 end_position = (x, y)
-            nodes[(x, y)] = cell
+            graph[(x, y)] = set()
 
-    for x, y in nodes:
-        for n in neighs(nodes, x, y):
-            edges.add(((x, y), n))
+    for x, y in graph:
+        for n in neighbours(graph, x, y):
+            graph[(x, y)].add(n)
 
-    return nodes, edges, start_position, end_position
+    return graph, start_position, end_position
 
 
-def neighs(nodes, x, y):
+def neighbours(graph, x, y):
     for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
         nx, ny = x + dx, y + dy
-        if (nx, ny) in nodes and nodes[(nx, ny)] != "#":
+        if (nx, ny) in graph:
             yield nx, ny
 
 
