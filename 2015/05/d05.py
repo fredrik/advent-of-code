1aa87567f40ca579b97972a3bb3457481d743c9d
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-03-25 13:15:15 +0100

    add 2015 d05 part 2

--- 2015/05/d05.py
+++ 2015/05/d05.py
@@ -1,32 +1,56 @@
+import re
 import os
+import itertools
 from string import ascii_lowercase
 
 
 def solve(data, part):
     count = 0
     for line in data.splitlines():
-        if nice(line):
+        if nice(line, part):
             count += 1
     return count
 
 
-def nice(line):
-    bad_strings = set(["ab", "cd", "pq", "xy"])
-    for s in bad_strings:
+def nice(line, part):
+    if part == 1:
+        return nice_part_one(line)
+    else:
+        return nice_part_two(line)
+
+
+def nice_part_one(line):
+    def twice(line):
+        for letter in ascii_lowercase:
+            if letter * 2 in line:
+                return True
+
+    def vowels(line):
+        vs = [x for x in line if x in set("aeiou")]
+        return len(vs) >= 3
+
+    for s in set(["ab", "cd", "pq", "xy"]):
         if s in line:
             return False
+
     return twice(line) and vowels(line)
 
 
-def twice(line):
-    for letter in ascii_lowercase:
-        if letter * 2 in line:
-            return True
+def nice_part_two(line):
+    def pair(line):
+        for t in itertools.product(ascii_lowercase, ascii_lowercase):
+            p = "".join(t)
+            pattern = re.compile(f"{p}.*{p}")
+            if pattern.search(line):
+                return True
 
+    def repeats(line):
+        for x in ascii_lowercase:
+            pattern = re.compile(f"{x}[a-z]{x}")
+            if pattern.search(line):
+                return True
 
-def vowels(line):
-    vs = [x for x in line if x in set("aeiou")]
-    return len(vs) >= 3
+    return pair(line) and repeats(line)
 
 
 # ---
@@ -47,4 +71,4 @@ def get_input_data():
 if __name__ == "__main__":
     data = get_input_data()
     print("part 1:", solve(data, 1))
-    # print("part 2:", solve(data, 2))
+    print("part 2:", solve(data, 2))
