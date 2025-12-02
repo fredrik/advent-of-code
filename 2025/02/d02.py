c1de48a3577846f3d3ca3aaa7590de4c317d5918
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-02 15:10:10 +0100

    Refactor 2025 d02 to part lookup in inner function

--- 2025/02/d02.py
+++ 2025/02/d02.py
@@ -3,20 +3,11 @@ import os
 
 
 def solve(input, part):
-    if part == 1:
-        answer = 0
-        for x in parse_input(input):
-            if repeats(x):
-                answer += x
-        return answer
-    else:
-        # Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice.
-        # So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
-        answer = 0
-        for x in parse_input(input):
-            if repeats_more(x):
-                answer += x
-        return answer
+    answer = 0
+    for x in parse_input(input):
+        if repeats(part, x):
+            answer += x
+    return answer
 
 
 def parse_input(input):
@@ -26,14 +17,9 @@ def parse_input(input):
             yield x  # todo: there's a neater way to 'yield from' somehow.
 
 
-def repeats(x: int):
-    """You can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice."""
-    return bool(re.search(r"^(\d+)\1$", str(x)))
-
-
-def repeats_more(x: int):
-    """You can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice."""
-    return bool(re.search(r"^(\d+)\1+$", str(x)))
+def repeats(part: int, x: int):
+    patterns = {1: r"^(\d+)\1$", 2: r"^(\d+)\1+$"}
+    return bool(re.search(patterns[part], str(x)))
 
 
 # ---
