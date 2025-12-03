b44a2ad316fc027cb3ab73cfd4e72096da1e41bf
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-03 08:46:39 +0100

    2025 d03 part 2

--- 2025/03/d03.py
+++ 2025/03/d03.py
@@ -1,24 +1,34 @@
 import os
 from itertools import combinations
 
+
 def solve(input, part):
+    banks = parse_input(input)
     if part == 1:
         jolts = 0
-        banks = parse_input(input)
         for bank in banks:
-            m = max([int(a+b) for a,b in combinations(bank, 2)])
-            print(f'b: {bank}, max: {m}')
+            m = max([int(a + b) for a, b in combinations(bank, 2)])
             jolts += m
         return jolts
+        # return sum(max(combinations(bank, 2)) for bank in banks)
     else:
-        return
+        jolts = 0
+        for bank in banks:
+            r = []
+            for i, d in enumerate(bank):
+                while r and r[-1] < d and len(r) + len(bank) - i > 12:
+                    r.pop()
+                if len(r) < 12:
+                    r.append(d)
+            jolts += int("".join(r))
+        return jolts
 
 
 def parse_input(input):
     for line in input:
         yield [x for x in line.strip()]
 
-``
+
 # ---
 
 
