ed334e3aa3b0bc52ce9b7d868970ebf981544b32
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-04 20:28:10 +0100

    2015 d14 part 2
    not sure about draws..

--- 2015/14/d14.py
+++ 2015/14/d14.py
@@ -1,15 +1,23 @@
 import re
 import os
 import sys
+from collections import defaultdict
+
 
 def solve(input, part):
-    reindeers = parse_input(input)
+    reindeers = list(parse_input(input))
     seconds = 2503
 
     if part == 1:
         return max(distance(reindeer, seconds) for reindeer in reindeers)
     else:
-        return
+        winners = defaultdict(int)
+        for s in range(1, seconds + 1):
+            w = winner(reindeers, s)
+            winners[w] += 1
+
+        return max(winners.values())
+
 
 def distance(reindeer, seconds):
     name, speed, duration, rest = reindeer
@@ -25,15 +33,28 @@ def distance(reindeer, seconds):
     return distance
 
 
+def winner(reindeers, s):
+    distances = {r[0]: distance(r, s) for r in reindeers}
+    return max(distances, key=distances.get)
+
+
 def parse_input(input):
     for line in input:
         match = re.search(
-        r"(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.",
-        line.strip(),
+            r"(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.",
+            line.strip(),
         )
-        name, speed, duration, rest = match[1], int(match[2]), int(match[3]), int(match[4])
+        name, speed, duration, rest = (
+            match[1],
+            int(match[2]),
+            int(match[3]),
+            int(match[4]),
+        )
         yield (name, speed, duration, rest)
+
+
 # ---
+
 
 def get_input():
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
