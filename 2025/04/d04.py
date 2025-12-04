120f0f5a03f07b6b80eff01f5feeb52bda6a6929
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-04 17:00:48 +0100

    Refactor 2025 d04

--- 2025/04/d04.py
+++ 2025/04/d04.py
@@ -6,46 +6,14 @@ def solve(input, part):
     grid = parse_input(input)
 
     if part == 1:
-        c = defaultdict(lambda: ".")
-        reachable = defaultdict(lambda: ".")
-        ok = 0
-        for i, j in list(grid):
-            if grid[(i, j)] != "@":
-                continue
-
-            adjacents = 0
-            for di in [-1, 0, 1]:
-                for dj in [-1, 0, 1]:
-                    if (di, dj) == (0, 0):
-                        continue
-                    if grid[(i + di, j + dj)] == "@":
-                        adjacents += 1
-
-            if adjacents < 4:
-                ok += 1
-                reachable[(i, j)] = "x"
-            else:
-                reachable[(i, j)] = grid[(i, j)]
-
-            c[(i, j)] = adjacents
-
-        #     print(f"i: {i}, j:{j}")
-        #     print(adjacents)
-
-        # print_grid(grid)
-        # print()
-        # print_grid(reachable)
-        # print()
-        # print_grid(c)
-
-        return ok
+        _, removed_count = remove(grid)
+        return removed_count
     else:
         total_removed = 0
-        new_grid, removed_count = remove(grid)
-        total_removed += removed_count
+        removed_count = None
 
         while removed_count != 0:
-            new_grid, removed_count = remove(new_grid)
+            grid, removed_count = remove(grid)
             total_removed += removed_count
 
         return total_removed
@@ -53,38 +21,27 @@ def solve(input, part):
 
 def remove(g):
     new_grid = defaultdict(str)
-    c = defaultdict(lambda: ".")
-    reachable = defaultdict(lambda: ".")
     ok = 0
     for i, j in list(g):
-        new_grid[(i, j)] = g[(i, j)]
         if g[(i, j)] != "@":
             continue
 
-        adjacents = 0
-        for di in [-1, 0, 1]:
-            for dj in [-1, 0, 1]:
-                if (di, dj) == (0, 0):
-                    continue
-                if g[(i + di, j + dj)] == "@":
-                    adjacents += 1
+        adjacents = sum(
+            [
+                True
+                for di in [-1, 0, 1]
+                for dj in [-1, 0, 1]
+                if (di, dj) != (0, 0) and g[(i + di, j + dj)] == "@"
+            ]
+        )
 
         if adjacents < 4:
             ok += 1
-            reachable[(i, j)] = "x"
             new_grid[(i, j)] = "."
         else:
-            reachable[(i, j)] = g[(i, j)]
-
-        c[(i, j)] = adjacents
+            new_grid[(i, j)] = g[(i, j)]
 
     return new_grid, ok
-
-
-def print_grid(c):
-    for i in range(10):
-        line = "".join([str(c[(i, j)]) for j in range(10)])
-        print(line)
 
 
 # ---
