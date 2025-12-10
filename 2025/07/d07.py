bb62d30877c7339775ed334b463f945d1baa7420
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-10 17:11:55 +0100

    aoc-vcr grid demo

--- 2025/07/d07.py
+++ 2025/07/d07.py
@@ -1,22 +1,26 @@
 from aoc import get_input
+from aoc_vcr import Recorder
 
 
 def solve(input, part):
     grid = parse_input(input)
 
     if part == 1:
-        counter = 0
-        for r, row in enumerate(grid):
-            for c, cell in enumerate(row):
-                if cell == "S":
-                    grid[r + 1][c] = "|"
-                if cell == "." and grid[r - 1][c] == "|":
-                    grid[r][c] = "|"
-                if cell == "^" and grid[r - 1][c] == "|":
-                    grid[r][c + 1] = "|"
-                    grid[r][c - 1] = "|"
-                    counter += 1
-        return counter
+        with Recorder(year=2025, day=7, part=1) as rec:
+            splits = 0
+            for r, row in enumerate(grid):
+                for c, cell in enumerate(row):
+                    if cell == "S":
+                        grid[r + 1][c] = "|"
+                    if cell == "." and grid[r - 1][c] == "|":
+                        grid[r][c] = "|"
+                    if cell == "^" and grid[r - 1][c] == "|":
+                        grid[r][c + 1] = "|"
+                        grid[r][c - 1] = "|"
+                        splits += 1
+
+                    rec.snapshot(grid=grid, splits=splits)
+            return splits
     else:
         return count_timelines(grid)
 
@@ -33,7 +37,6 @@ def count_timelines(grid, start_r=0, start_c=0):
             if cell == "." and grid[r - 1][c] == "|":
                 grid[r][c] = "|"
             if cell == "^" and grid[r - 1][c] == "|":
-
                 left_copy = [row[:] for row in grid]
                 left_copy[r][c - 1] = "|"
                 left_key = (r, c, tuple(left_copy[r]))
