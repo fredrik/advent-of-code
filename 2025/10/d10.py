0187240f42648b77ff637420d270bfea21136262
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2025-12-11 22:22:48 +0100

    2025 d10 part 2
    with scipy :(

--- 2025/10/d10.py
+++ 2025/10/d10.py
@@ -1,12 +1,14 @@
+from aoc import get_input
 from collections import deque
 import re
-from aoc import get_input
-from functools import cache
+
+
+# :(
+from scipy.optimize import milp, LinearConstraint, Bounds
+import numpy as np
 
 
 def solve(input, part):
-    machines = list(parse_input(input))
-
     # for target, toggles in machines:
     #     print(
     #         format(target, '08b'),
@@ -16,11 +18,41 @@ def solve(input, part):
     #     )
 
     if part == 1:
+        machines = list(parse_input(input))
         return sum(presses(0, target, toggles) for target, toggles in machines)
     else:
-        return
+        machines = parse_input2(input)
+
+        # for joltages, toggles in machines:
+        #     print(joltages, toggles)
+        #     # least = least_joltage_presses(
+        #     #     (0,) * len(joltages),
+        #     #     joltages,
+        #     #     toggles)
+        #     # print(least)
+        #     print('-------------')
+        #     print()
+
+        return sum(ilp(target, toggles) for target, toggles in machines)
 
 
+def ilp(target, toggles):
+    toggles = np.array(toggles)
+    target = np.array(target)
+
+    result = milp(
+        c=np.ones(len(toggles)),
+        constraints=LinearConstraint(toggles.T, target, target),
+        bounds=Bounds(0, np.inf),
+        integrality=np.ones(len(toggles)),
+    )
+
+    if result.success:
+        return int(result.fun)
+    return 0
+
+
+# "least" presses!
 def presses(state, target, toggles):
     if state == target:
         return 0
@@ -53,7 +85,7 @@ def parse_input(input):
         return int("".join("1" if b else "0" for b in bitmask), 2)
 
     for line in input:
-        match = re.search(r"\[(.+)\] (.*) \{", line)
+        match = re.search(r"\[(.+)\] (.*) \{(.*)\}", line)
         if match:
             lights = parse_lights(match.group(1))
             toggles = [
@@ -64,6 +96,38 @@ def parse_input(input):
             # lights and toggles are bit masks. convert to binary.
             # [False, True, True, False] => 0110
             yield (to_binary(lights), tuple(to_binary(t) for t in toggles))
+
+
+def parse_input2(input):
+    def parse_lights(str):
+        return [True if s == "#" else False for s in str]
+
+    def parse_toggles(str):
+        return [
+            tuple(map(int, s.split(",")))
+            for s in str.replace("(", "").replace(")", "").split(" ")
+        ]
+
+    def parse_joltages(str):
+        return tuple(int(s.strip()) for s in str.split(","))
+
+    def to_binary(bitmask):
+        return int("".join("1" if b else "0" for b in bitmask), 2)
+
+    for line in input:
+        match = re.search(r"\[(.+)\] (.*) \{(.*)\}", line)
+        if match:
+            lights = parse_lights(match.group(1))
+            # toggles = parse_toggles(match.group(2))
+            toggles = tuple(
+                tuple([1 if i in positions else 0 for i in range(len(lights))])
+                for positions in parse_toggles(match.group(2))
+            )
+            joltages = parse_joltages(match.group(3))
+
+            # lights and toggles are bit masks. convert to binary.
+            # [False, True, True, False] => 0110
+            yield (joltages, toggles)
 
 
 if __name__ == "__main__":
